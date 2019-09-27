d332e13d9584781b1d1409fa93509bc0e98679ee
Rose Seraphin <roseseraphin@yahoo.com>
2019-09-27 01:00:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,13 +11,13 @@ array.reverse
 end
 
 def using_first(array)
-array.using_first
+array.first
 end
 
 def using_last(array)
-array.using_last
+array.last
 end
 
 def using_size(array)
-array.using_size
+array.size
 end
